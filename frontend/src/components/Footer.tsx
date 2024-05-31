096a5b6c743bc4b840b817f55671a4556e23a1f8
Agustin-desktop <agustin2051@gmail.com>
2024-05-31 13:10:21 -0300

    Routesfix

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,12 +2,12 @@ import Terms from "./Terms";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#010101] py-5 -mb-8 w-full">
+    <footer className="bg-[#8DB600] py-5 -mb-8 w-full">
       <div className="mt-8 text-center">
-        <p className="text-white">
+        <p className="text-black">
           2024 &copy; WoodWork. Todos los derechos reservados.
         </p>
-        <p className="text-white font-thin">No-Country | s15-17-t</p>
+        <p className="text-black font-thin">No-Country | s15-17-t</p>
         <Terms />
       </div>
     </footer>
