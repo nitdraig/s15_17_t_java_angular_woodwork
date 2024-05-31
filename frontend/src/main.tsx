40256defa518dc6b3f543417ade2190782cc0aa2
Agustin-desktop <agustin2051@gmail.com>
2024-05-31 18:39:53 -0300

    Favicon

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; // Importa ReactDOM de esta manera
-import App from './App.tsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.tsx";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
