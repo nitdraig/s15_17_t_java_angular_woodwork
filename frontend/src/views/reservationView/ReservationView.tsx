ca40d6c6bcd35523d6fd0ec7aaab3d50e80e65fd
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2024-05-31 21:22:44 -0300

    add: card reservation

--- frontend/src/views/reservationView/ReservationView.tsx
+++ frontend/src/views/reservationView/ReservationView.tsx
@@ -1,65 +1,53 @@
-
 const ReservationView = () => {
   return (
-    <div className="relative bg-white p-8">
-      {/* Contenedor principal */}
-      <div className="relative w-96 h-96 bg-lime-900 rounded-lg shadow-md mx-auto">
-        {/* Contenedor interior 1 */}
-        <div className="absolute inset-0 w-96 h-36 bg-white rounded-lg m-6" />
-        <div className="absolute inset-0 w-96 h-80 bg-lime-900 border-2 border-stone-800 m-6" />
-        <div className="absolute top-26 left-80 w-48 h-16 bg-zinc-500 rounded-lg" />
-        <div className="absolute bottom-0 left-80 w-48 h-16 bg-zinc-500 rounded-lg" />
-        <div className="absolute top-2244 left-80 w-96 h-96 bg-neutral-400 rounded-lg" />
-        <div className="absolute top-2262 left-115 w-72 h-16 text-white text-3xl font-normal tracking-wider">$5.000 por persona</div>
-        <div className="absolute bottom-0 left-80 w-48 h-16 bg-zinc-500 rounded-lg" />
-        <div className="absolute bottom-100 left-80 w-48 h-16 bg-zinc-500 rounded-lg" />
-        <div className="absolute top-26 right-0 w-32 h-16 bg-lime-300 rounded-lg" />
-        <div className="absolute top-1241 right-0 w-96 h-16 bg-yellow-300 rounded-lg" />
-        <div className="absolute top-239 right-0 w-48 h-16 bg-orange-400 rounded-lg" />
-        <div className="absolute top-253 right-0 text-white text-3xl font-normal tracking-wider">filtros</div>
-        <div className="absolute top-141 left-299 w-96 h-16 bg-orange-100 rounded-lg" />
-        <div className="absolute top-256 left-79 text-zinc-300 text-3xl font-normal tracking-wider">Full Nature</div>
-        <div className="absolute top-256 left-444 text-zinc-300 text-3xl font-normal tracking-wider">In group</div>
-        <div className="absolute top-256 left-282 text-zinc-300 text-3xl font-normal tracking-wider">Creative</div>
-        <div className="absolute top-256 left-613 text-zinc-300 text-3xl font-normal tracking-wider">Serenity</div>
-        <div className="absolute top-256 left-777 text-zinc-300 text-3xl font-normal tracking-wider">Filtro</div>
-        <div className="absolute top-256 left-902 text-zinc-300 text-3xl font-normal tracking-wider">filtro</div>
-        <div className="absolute top-256 left-1010 text-zinc-300 text-3xl font-normal tracking-wider">filtro</div>
-        <div className="absolute origin-top-left rotate-180 w-8 h-8 left-1139 top-256 bg-zinc-300 rounded-full" />
-        <img className="absolute top-490 left-79 w-96 h-96 rounded-lg" src="https://via.placeholder.com/845x514" />
-        <img className="absolute top-490 left-955 w-96 h-64 rounded-lg" src="https://via.placeholder.com/405x249" />
-        <img className="absolute top-755 left-955 w-96 h-64 rounded-lg" src="https://via.placeholder.com/405x249" />
-        <div className="absolute top-373 left-87 text-zinc-900 text-6xl font-semibold tracking-widest">Work Together</div>
-        <div className="absolute top-1048 left-80 text-zinc-900 text-6xl font-semibold tracking-widest">Work Together</div>
-        <div className="absolute top-1139 left-79 text-zinc-500 text-3xl font-normal tracking-wider">Tagle 3000</div>
-        <div className="absolute top-1916 left-79 text-zinc-500 text-3xl font-normal tracking-wider">Descripcion del coworking</div>
-        <div className="absolute top-1213 left-79 w-96 h-36 bg-neutral-500 opacity-60 rounded-lg" />
-        <div className="absolute top-1259 left-111 text-center text-zinc-900 text-base font-semibold tracking-wide">Favorito entre<br />los freelancers</div>
-        <div className="absolute top-1259 left-299 w-48 text-center text-zinc-900 text-base font-semibold tracking-wide">Uno de los mejores<br />CoWorks del mes</div>
-        <div className="absolute top-1575 left-299 w-48 text-center text-zinc-900 text-base font-semibold tracking-wide">Servicio</div>
-        <div className="absolute top-1988 left-80 w-96 text-zinc-900 text-base font-normal tracking-wide">
-          Y, viéndole don Quijote de aquella manera, con muestras de tanta tristeza, le dijo: Sábete, Sancho, que no es un hombre más que otro si no hace más que otro. Todas estas borrascas que nos suceden son señales de que presto ha de serenar el tiempo y han de sucedernos bien las cosas; porque no es posible que el mal ni el bien sean durables, y de aquí se sigue que, habiendo durado mucho el mal, el bien está ya cerca. Así que, no debes congojarte por las desgracias que a mí me suceden, pues a ti no te cabe parte dellas.
-        </div>
-        <div className="absolute top-1674 left-299 w-48 text-center text-zinc-900 text-base font-semibold tracking-wide">Servicio</div>
-        <div className="absolute top-1773 left-299 w-48 text-center text-zinc-900 text-base font-semibold tracking-wide">Servicio</div>
-        <div className="absolute top-1271 left-771 text-zinc-900 text-base font-semibold tracking-wide">Opiniones</div>
-        <div className="absolute top-1299 left-587 w-20 h-16 text-zinc-900 text-3xl font-normal tracking-wider">4.89</div>
-        <div className="absolute origin-top-left rotate-180 w-8 h-8 left-896 top-1267 bg-zinc-300 rounded-full" />
-        <div className="absolute top-1326 right-0 w-72 h-16 text-white text-3xl font-normal tracking-wider"> $XXXXX</div>
-        <div className="absolute top-1394 right-0 w-72 h-16 text-white text-3xl font-normal tracking-wider"> $XXXXX</div>
-        <div className="absolute top-1472 right-0 w-72 h-16 text-white text-3xl font-normal tracking-wider"> Total $XXXXX</div>
-        <div className="absolute top-1330 right-0 w-72 h-16 text-white text-3xl font-normal tracking-wider">x5 personas</div>
-        <div className="absolute top-1394 right-0 w-72 h-16 text-white text-3xl font-normal tracking-wider">x7 días</div>
-        <div className="absolute top-1798.19 right-0 w-28 h-6 text-white text-base font-normal tracking-wide">$XXX x5 días</div>
-        <div className="absolute top-1842.08 right-0 w-24 h-6 text-white text-base font-normal tracking-wide">x4 personas</div>
-        <div className="absolute top-1922.11 right-0 w-11 h-6 text-white text-base font-bold tracking-wide">Total</div>
-        <div className="absolute top-1798.19 right-0 w-20 h-6 text-white text-base font-normal tracking-wide">$XXXXXX </div>
-        <div className="absolute top-1844.66 right-0 w-20 h-6 text-white text-base font-normal tracking-wide">$XXXXXX</div>
-        <div className="absolute top-1922.11 right-0 w-20 h-6 text-white text-base font-normal tracking-wide">$XXXXXX</div>
-        <div className="absolute top-1253 left-1104 text-black text-3xl font-normal">Reservar</div>
-        <div className="absolute top-1459 left-0 w-36 h-12 text-black text-base font-normal tracking-wide">Elige el lugar que desees</div>
+    <>
+      <div className="pl-12 pb-6 pt-12 text-zinc-900 text-6xl font-bold font-['Kanit'] tracking-wider">
+        Work Together
       </div>
-    </div>
+      <div className="flex px-12 mb-8">
+        <div className="w-3/5 flex items-stretch pr-5">
+          <img className="w-full rounded-lg object-cover" src="src/assets/imgDashboard.png" alt="Work Together" />
+        </div>
+        <div className="w-2/5 flex flex-col justify-between space-y-4">
+          <img className="w-full h-1/2 rounded-lg object-cover" src="src/assets/imgDashboard.png" alt="Work Together" />
+          <img className="w-full h-1/2 rounded-lg object-cover" src="src/assets/imgDashboard.png" alt="Work Together" />
+        </div>
+      </div>
+      
+      <div className="flex px-12">
+        <div className="w-3/5 text-zinc-900 text-6xl font-bold font-['Kanit'] tracking-wider">
+          Work Together
+        </div>
+        <div className="w-2/5 flex flex-col space-y-4 p-8 bg-[#323E1D] rounded-lg shadow-md">
+          <img className="w-full h-32 rounded-lg object-cover" src="src/assets/imgDashboard.png" alt="Small Image" />
+          <button
+            type="submit"
+            className="w-full bg-[#F9EC34] hover:bg-[#A67C52] focus:ring-4 focus:outline-none focus:ring-[#31543D] font-medium rounded-lg text-sm md:text-lg px-5 py-2.5 text-center shadow-md hover:shadow-lg transition duration-150 ease-in-out"
+          >
+            Reservar
+          </button>
+          <div className="flex flex-col space-y-2 mt-4">
+            <div className="flex justify-between">
+              <span className="font-semibold text-white">Cantidad de personas:</span>
+              <span className="text-white">4</span>
+            </div>
+            <div className="flex justify-between">
+              <span className="font-semibold text-white">Cantidad de horas:</span>
+              <span className="text-white">3</span>
+            </div>
+            <div className="flex justify-between">
+              <span className="font-semibold text-white">Cantidad de días:</span>
+              <span className="text-white">2</span>
+            </div>
+            <hr className="border-t border-white my-4" />
+            <div className="flex justify-between">
+              <span className="font-semibold text-white">Total:</span>
+              <span className="text-white">$XXXXX</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
