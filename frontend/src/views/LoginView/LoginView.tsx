38dd1c5991777e5844d503493fc06613f1a2999e
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2024-06-01 12:04:17 -0300

    add bg in register and login

--- frontend/src/views/LoginView/LoginView.tsx
+++ frontend/src/views/LoginView/LoginView.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "./LoginForm/LoginForm";
 
 const LoginView = () => {
   return (
-    <section className="bg-[#31543D] min-h-screen flex items-center">
+    <section className="bg-gradient-to-r from-green-700 via-green-600 to-green-500 min-h-screen flex items-center">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto w-full md:w-70 lg:w-3/5 xl:w-2/5 md:items-end">
         <div className="w-full bg-white rounded-lg shadow-lg md:mt-0 xl:p-0">
           <div className="p-6 space-y-1 md:space-y-3 sm:p-8">
