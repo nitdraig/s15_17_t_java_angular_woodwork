f6a4487522c095fdcaba38d4982fbdefb5f9eb8b
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-06-02 14:32:55 -0300

    Update WorkspaceEntity.java

--- backend/coworkProject/src/main/java/org/example/coworkproject/entity/WorkspaceEntity.java
+++ backend/coworkProject/src/main/java/org/example/coworkproject/entity/WorkspaceEntity.java
@@ -1,10 +1,16 @@
 package org.example.coworkproject.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
+import org.example.coworkproject.controller.ReservationController;
 
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -16,16 +22,22 @@ public class WorkspaceEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_workspace;
 
-    @ElementCollection(fetch = FetchType.LAZY)
-    @CollectionTable
-    private List<String> days;
-
-    private String startTime;
-    private String endTime;
     private String workspaceName;
     private String address;
-    private Float pricePerHour;
+    private double pricePerHour;
     private String description;
+    private int capacity;
+
+    @ElementCollection
+    @Enumerated(EnumType.STRING)
+    private Set<DayOfWeek> openDays;
+
+    private LocalTime openingTime;
+    private LocalTime closingTime;
+
+    @OneToMany(mappedBy = "workspace")
+    @JsonManagedReference
+    private List<ReservationEntity> reservations;
 
     @ManyToMany(mappedBy = "workspaces")
     @JsonIgnoreProperties("workspaces")
