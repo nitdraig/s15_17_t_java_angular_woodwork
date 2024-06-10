f4f664750b75448b1413b3bfecdd3ee855422a72
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-06-10 17:27:01 -0300

    Update LoginRequestDTO.java

--- backend/coworkProject/src/main/java/org/example/coworkproject/dto/request/LoginRequestDTO.java
+++ backend/coworkProject/src/main/java/org/example/coworkproject/dto/request/LoginRequestDTO.java
@@ -17,7 +17,8 @@ public class LoginRequestDTO {
 
     @NotBlank(message = "Email is required.")
     @Email(message = "Invalid email address.")
-    @Size(max = 50, message = "Email must not exceed 50 characters ")
+    @Size(max = 50, message = "Email must not exceed 50 characters.")
+    @Pattern(regexp = ".+@.+\\.[a-zA-Z]{2,}", message = "Email should have a valid domain with at least two characters.")
     private String email;
 
     @NotBlank(message = "Password is required.")
