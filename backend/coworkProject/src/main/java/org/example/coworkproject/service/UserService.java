7300ccf1b22a70fe058eb712c66898eaddf1548c
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-06-10 18:00:58 -0300

    Update UserService.java

--- backend/coworkProject/src/main/java/org/example/coworkproject/service/UserService.java
+++ backend/coworkProject/src/main/java/org/example/coworkproject/service/UserService.java
@@ -1,6 +1,7 @@
 package org.example.coworkproject.service;
 
 
+import org.example.coworkproject.dto.request.ChangePasswordRequestDTO;
 import org.example.coworkproject.dto.response.UserResponseDTO;
 import org.example.coworkproject.exception.MyException;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,7 +10,7 @@ import java.util.List;
 
 public interface UserService {
 
-    UserResponseDTO changeUserPassword(Long id_user, String password) throws MyException;
+    UserResponseDTO changeUserPassword(Long id_user, ChangePasswordRequestDTO changePasswordRequestDTO) throws MyException;
     List<UserResponseDTO> getAllUsers();
     UserResponseDTO findUserById(Long id_user);
     UserResponseDTO updateUser(Long id_user, String fullName, MultipartFile profilePicture);
