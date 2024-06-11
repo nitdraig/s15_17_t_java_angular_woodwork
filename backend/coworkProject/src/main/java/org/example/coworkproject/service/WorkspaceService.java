4fdccfb09c1edbb82c25485f9e7e167ce028b406
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-06-11 19:30:43 -0300

    Update WorkspaceService.java

--- backend/coworkProject/src/main/java/org/example/coworkproject/service/WorkspaceService.java
+++ backend/coworkProject/src/main/java/org/example/coworkproject/service/WorkspaceService.java
@@ -2,6 +2,7 @@ package org.example.coworkproject.service;
 
 import io.jsonwebtoken.io.IOException;
 import org.example.coworkproject.dto.request.WorkspaceRequestDTO;
+import org.example.coworkproject.dto.response.QualitiesResponseDTO;
 import org.example.coworkproject.dto.response.WorkspaceResponseDTO;
 import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
@@ -18,4 +19,5 @@ public interface WorkspaceService {
     WorkspaceResponseDTO addImages(Long id_workspace, List<MultipartFile> images) throws IOException;
     WorkspaceResponseDTO addMainImage(Long id_workspace, MultipartFile mainImage) throws IOException;
     WorkspaceResponseDTO deleteWorkspace(Long id);
+    List<QualitiesResponseDTO> getQualitiesByWorkspace(Long id_workspace);
 }
